0ec6737044e5331441b612798f72e19281cfe1c7
datadominik <dominik.schniersthauer@googlemail.com>
2020-04-06 01:50:46 +0000

    preparation for project submission

---
+++ src/model_new.py
@@ -0,0 +1,156 @@
+import os
+import numpy as np
+from argparse import ArgumentParser
+import tensorflow as tf
+import logging
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, BatchNormalization, Conv2D, Dense, Dropout, Flatten,  MaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import categorical_crossentropy
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications.densenet import DenseNet121
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
+from sklearn.utils import class_weight
+
+BATCH_SIZE = 16
+    
+def keras_model_fn():
+    model_pretrained = DenseNet121(include_top=False, weights='imagenet', input_shape=(224,224,3))
+    
+    model = Sequential()
+#     for layer in model_pretrained.layers:
+#         layer.trainable = False
+        
+    model.add(model_pretrained)
+    model.add(GlobalAveragePooling2D())
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(256, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(4, activation='softmax'))
+    
+    optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
+    
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=['accuracy'])
+    return model
+
+def serving_input_fn(hyperparameters):
+    tensor = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
+    inputs = {"vgg16_input": tensor}
+    return tf.estimator.export.ServingInputReceiver(inputs, inputs)
+
+def train_input_fn(training_dir):
+    return _input(tf.estimator.ModeKeys.TRAIN, batch_size=BATCH_SIZE, data_dir=training_dir)
+
+
+def eval_input_fn(training_dir):
+    return _input(tf.estimator.ModeKeys.EVAL, batch_size=BATCH_SIZE, data_dir=training_dir)
+
+
+def _input(mode, batch_size, data_dir):
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        img_generator = ImageDataGenerator(rescale=1./255, rotation_range=360,
+                        width_shift_range=0.2, 
+                        height_shift_range=0.2, 
+                        zoom_range=0.3, 
+                        horizontal_flip=True, 
+                        vertical_flip=True)
+    elif mode == tf.estimator.ModeKeys.EVAL:
+        img_generator = ImageDataGenerator(rescale=1./255)
+        
+    generator = img_generator.flow_from_directory(data_dir, target_size=(224, 224), batch_size=batch_size)
+
+    return generator
+
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--train',
+        type=str,
+        required=False,
+        default=os.environ.get('SM_CHANNEL_TRAIN'),
+        help='Directory for training data.')
+    parser.add_argument(
+        '--validation',
+        type=str,
+        required=False,
+        default=os.environ.get('SM_CHANNEL_VALIDATION'),
+        help='Directory for validation data.')
+    parser.add_argument(
+        '--eval',
+        type=str,
+        required=False,
+        default=os.environ.get('SM_CHANNEL_EVAL'),
+        help='Directory for evaluation data.')
+    parser.add_argument(
+        '--model_dir',
+        type=str,
+        required=True,
+        help='Model storage directory.')
+    parser.add_argument(
+        '--model_output_dir',
+        type=str,
+        default=os.environ.get('SM_MODEL_DIR'))
+    parser.add_argument(
+        '--output-dir',
+        type=str,
+        default=os.environ.get('SM_OUTPUT_DIR'))
+    parser.add_argument(
+        '--epochs',
+        type=int,
+        default=10,
+        help='Number of training epochs.')
+    parser.add_argument(
+        '--batch-size',
+        type=int,
+        default=16,
+        help='Training batch size.')
+
+    
+    args = parser.parse_args()
+    train_path = args.train
+    val_path = args.validation
+    model_dir = args.model_dir
+    
+    train_generator = train_input_fn(os.environ.get('SM_CHANNEL_TRAIN'))
+    val_generator = eval_input_fn(os.environ.get('SM_CHANNEL_VAL'))
+    
+    STEPS_TRAIN=(train_generator.n//train_generator.batch_size) * 3
+    STEPS_VAL=val_generator.n//val_generator.batch_size
+    
+    X_train, y_train = train_generator.next()
+    for step in range(STEPS_TRAIN-1):
+        X_tmp, y_tmp = train_generator.next()
+        X_train = np.vstack([X_train, X_tmp])
+        y_train = np.vstack([y_train, y_tmp])
+    
+    X_val, y_val = val_generator.next()
+    for step in range(STEPS_VAL-1):
+        X_tmp, y_tmp = val_generator.next()
+        X_val = np.vstack([X_val, X_tmp])
+        y_val = np.vstack([y_val, y_tmp])
+        
+    model = keras_model_fn()
+    
+    annealer = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=5, verbose=1, min_lr=1e-4)
+    checkpoint = ModelCheckpoint(args.output_dir + '/checkpoint.h5', monitor='val_loss', verbose=1, save_best_only=True)
+    callbacks = [annealer, checkpoint]
+    
+    class_weights = {0: 1.,
+                    1: 3.,
+                    2: 1.,
+                    3: 1.}    
+    model.fit(X_train, y_train, epochs=50, validation_data=(X_val, y_val), callbacks=callbacks, class_weight=class_weights, shuffle=True, verbose=2)
+    
+    model = tf.keras.models.load_model(args.output_dir + '/checkpoint.h5')
+    tf.contrib.saved_model.save_keras_model(model, args.model_dir)
+    tf.contrib.saved_model.save_keras_model(model, args.model_output_dir)
+    logging.info("Model successfully saved at: {}".format(args.model_output_dir))
--- src/project_utils.py
+++ src/project_utils.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from imageio import imread
+from shutil import copyfile
+from sklearn.model_selection import train_test_split
 from PIL import Image
+import os
 
 def plot_examples(df, _target_class="healthy", _rows=3, _columns=4, _figsize=(20,9)):
     
@@ -23,10 +26,72 @@ def plot_examples(df, _target_class="healthy", _rows=3, _columns=4, _figsize=(20
         plt.axis('off')
         
     #plt.tight_layout()
-    fig.suptitle("Target class: {}".format(_target_class), fontsize=20)
+    #fig.suptitle("Target class: {}".format(_target_class), fontsize=20)
     plt.show()
 
+def plot_hist(df, _target_class="healthy", _rows=3, _columns=4, _figsize=(20,9)):
+    # most logic from https://discuss.pytorch.org/t/plot-a-histogram-for-multiple-images-full-dataset/67600/15
+    df_filtered = df[df[_target_class] == 1]
 
+    fig = plt.figure(figsize=_figsize)
+    
+    columns = _columns
+    rows = _rows
+    
+    img_idx_list = np.random.randint(0, len(df_filtered), int(columns * rows))
+    
+    for i in range(1, columns*rows +1):
+        fig.add_subplot(rows, columns, i)
+        img_name = df_filtered.iloc[img_idx_list[i-1]].image_id
+        img = imread("images/{}.jpg".format(img_name))
+        plt.hist(img[:, :, 0].ravel(), bins = 256, color = 'Red', alpha = 0.5)
+        plt.hist(img[:, :, 1].ravel(), bins = 256, color = 'Green', alpha = 0.5)
+        plt.hist(img[:, :, 2].ravel(), bins = 256, color = 'Blue', alpha = 0.5)
+        plt.xlabel('intensity')
+        plt.ylabel('count')
+        plt.title(str(img_name))
+        
+    
+    
+    plt.legend(['Red_Channel', 'Green_Channel', 'Blue_Channel'], loc='upper center', bbox_to_anchor=(0.5, -0.2),fancybox=False, shadow=False)
+
+    plt.tight_layout()
+#     fig.suptitle("Target class: {}".format(_target_class), fontsize=20)
+    plt.show()
+    
+    
+def plot_agg_hist(df, _target_class="healthy"):
+    
+    # most logic from https://discuss.pytorch.org/t/plot-a-histogram-for-multiple-images-full-dataset/67600/15
+    
+    target_list = "./images/" + df[df[_target_class]==1].image_id.values + ".jpg"
+
+    nb_bins = 256
+    count_r = np.zeros(nb_bins)
+    count_g = np.zeros(nb_bins)
+    count_b = np.zeros(nb_bins)
+
+    for image in target_list:
+        img = Image.open(image)
+        x = np.array(img)
+        x = x.transpose(2, 0, 1)
+        hist_r = np.histogram(x[0], bins=nb_bins, range=[0, 255])
+        hist_g = np.histogram(x[1], bins=nb_bins, range=[0, 255])
+        hist_b = np.histogram(x[2], bins=nb_bins, range=[0, 255])
+        count_r += hist_r[0]
+        count_g += hist_g[0]
+        count_b += hist_b[0]
+
+    bins = hist_r[1]
+    fig = plt.figure()
+    plt.bar(bins[:-1], count_r, color='r', alpha=0.7)
+    plt.bar(bins[:-1], count_g, color='g', alpha=0.7)
+    plt.bar(bins[:-1], count_b, color='b', alpha=0.7)
+    plt.xlabel('intensity')
+    plt.ylabel('count')
+    plt.title(_target_class)
+    plt.show()
+    
 def plot_patches(patches_list, _rows=10, _columns=10, _figsize=(15,15)):
     
     fig = plt.figure(figsize=_figsize)
@@ -51,3 +116,37 @@ def load_images(df, size=(224,224)):
     return img_list
     
     
+def change_file_structure(df, src_dir="images", tar_dir="train_data", pred_list=["healthy", "multiple_diseases", "rust", "scab"], val_size=0.2):
+    
+    train_dir = os.path.join(tar_dir, "train")
+    val_dir = os.path.join(tar_dir, "val")
+    
+    for dir_ in [tar_dir, train_dir, val_dir]:
+        if not os.path.exists(dir_):
+            os.mkdir(dir_)
+    
+    for pred in pred_list:
+        if not os.path.exists(os.path.join(train_dir, pred)):
+            os.mkdir(os.path.join(train_dir, pred))
+
+        if not os.path.exists(os.path.join(val_dir, pred)):
+            os.mkdir(os.path.join(val_dir, pred)) 
+            
+    df.index = df.image_id
+    del df["image_id"]
+    df["pred"] = df[pred_list].idxmax(axis=1)
+
+    preds = df.pred.values
+    images = df.index.values
+    
+    img_train, img_val, pred_train, pred_val = train_test_split(images, preds, stratify=preds, test_size=0.2)
+    
+    for img_id, img_pred in zip(img_train, pred_train):
+        src_path = os.path.join(src_dir, img_id+".jpg")
+        tar_path = os.path.join(tar_dir, "train", img_pred, img_id+".jpg")
+        copyfile(src_path, tar_path)
+    
+    for img_id, img_pred in zip(img_val, pred_val):
+        src_path = os.path.join(src_dir, img_id+".jpg")
+        tar_path = os.path.join(tar_dir, "val", img_pred, img_id+".jpg")
+        copyfile(src_path, tar_path)
